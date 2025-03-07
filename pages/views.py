e14291ebf6769ab63aec5b6b74a4de34a366fdf9
Adel <musin.f.mail@gmail.com>
2025-03-07 16:03:21 +0300

    new teplates

--- pages/views.py
+++ pages/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+# from django.http import HttpResponse
+
+
+# def home_page_view(request):
+#     return HttpResponse("Hompage")
 
 
 def home_page_view(request):
-    return HttpResponse("Hompage")
-
-
-def about_page_view(request):
-    context = {
-        "name": "word",
-        "second": 6,
-    }
-    return render(request, "pages/about.html", context)
+    context = {}
+    return render(request, "home.html")
