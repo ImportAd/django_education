7ee480de1c28deb135cc71a16d4bfe87df61c680
Adel <musin.f.mail@gmail.com>
2025-03-10 11:39:25 +0300

    cleaning

--- pages/views.py
+++ pages/views.py
@@ -8,5 +8,8 @@ from django.shortcuts import render
 
 
 def home_page_view(request):
-    context = {}
-    return render(request, "home.html")
+    context = {
+        "inventory_list": ["Widget 1", "Widget 2", "Widget 3"],
+        "greeting": "THAnk you FOR visitING.",
+    }
+    return render(request, "home.html", context)
