440c6678fa5342d397a433cd6f39b983ba9eeb01
Adel <musin.f.mail@gmail.com>
2025-03-11 11:05:24 +0300

    class-based views

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # from django.http import HttpResponse
 
@@ -13,3 +14,13 @@ def home_page_view(request):
         "greeting": "THAnk you FOR visitING.",
     }
     return render(request, "home.html", context)
+
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["contact_address"] = "123 Main Street"
+        context["phone_number"] = "8-800-555-35-35"
+        return context
