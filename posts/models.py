1b67352b0ea81657a2867595461bc835de80f8f7
Adel <musin.f.mail@gmail.com>
2025-03-12 12:44:13 +0300

    add discription

--- posts/models.py
+++ posts/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 
 class Post(models.Model):
+    """Создаю модель описывающую один пост
+    Пост имеет поля:
+    - text
+    """
+
     text = models.TextField()
 
     def __str__(self):
