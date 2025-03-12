6e897893a3a56b2aff704c80a954be99b0cd61dd
Adel <musin.f.mail@gmail.com>
2025-03-12 13:35:44 +0300

    add new test

--- posts/tests.py
+++ posts/tests.py
@@ -2,6 +2,8 @@ from django.test import TestCase
 
 from .models import Post
 
+from django.urls import reverse
+
 
 class PostTests(TestCase):
     @classmethod
@@ -10,3 +12,12 @@ class PostTests(TestCase):
 
     def test_model_content(self):
         self.assertEqual(self.post.text, "This is a test!")
+
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/")
+
+    def test_post_page(self):
+        response = self.client.get(reverse("post"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "post_list.html")
+        self.assertContains(response, "This is a test!")
