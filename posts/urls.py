4087e96544b42e72167577d2ff04fb1c094e63d7
Adel <musin.f.mail@gmail.com>
2025-03-12 12:51:12 +0300

    add ListViwe

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import post_list
+
+# from .views import post_list
+from .views import PostList
 
 urlpatterns = [
-    path("post/", post_list, name="post"),
+    # path("post/", post_list, name="post"),
+    path("post/", PostList.as_view(), name="post")
 ]
