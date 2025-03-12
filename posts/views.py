c536a287051fb276272ee793893525c88d424114
Adel <musin.f.mail@gmail.com>
2025-03-12 12:48:23 +0300

    add ListView

--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+# from .models import Post
+
+
+# def post_list(request):
+#     posts = Post.objects.all()
+#     return render(
+#         request=request,
+#         template_name="post_list.html",
+#         context={"posts": posts},
+#     )
+
+from django.views.generic import ListView
 from .models import Post
 
 
-def post_list(request):
-    posts = Post.objects.all()
-    return render(
-        request=request,
-        template_name="post_list.html",
-        context={"posts": posts},
-    )
+class PostList(ListView):
+    model = Post
+    template_name = "post_list.html"
