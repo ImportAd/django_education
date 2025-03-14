73d220301295bdfcaecbc5a7b90d665ffa00feed
Adel <musin.f.mail@gmail.com>
2025-03-14 10:23:17 +0300

    add blog view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(
+        request=request,
+        template_name="blog.html",
+        context={"posts": posts},
+    )
